66995388513892357b2a511a3c82af63ee62947a
Antony Kotlyakov <akotlyakov@insquad.com>
2023-11-23 16:17:22 +0300

    init client

---
+++ client/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: {
+    dirs: ['app', 'components', 'utils', 'packages', 'hooks', 'hocs', 'redux'],
+  },
+  sassOptions: {
+    includePaths: ['./src'],
+    prependData: `
+      @import 'styles/breakpoints.scss';
+      @import 'styles/mixins.scss';
+    `,
+  },}
+
+module.exports = nextConfig
